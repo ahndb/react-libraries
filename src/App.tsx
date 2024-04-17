ecc951c5afa714e52cf84d6ecc778ce7b5b5a344
ahndb <asi0000@nate.com>
2024-04-17 11:52:05 +0900

    240417 11:50 Update ReactRouterLibrary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ZustandLibrary from 'src/zustand/ZustandLibrary';
+import ReactRouterLibrary from './react-router/ReactRouterLibrary';
 
 function App() {
   return (
     <>
       <ZustandLibrary />
+      <ReactRouterLibrary />
     </>
   );
 }
--- src/zustand/ZustandLibrary.tsx
+++ src/zustand/ZustandLibrary.tsx
@@ -75,16 +75,49 @@ export default function ZustandLibrary() {
 //          render          //
   return (
     <div>
+      <a href="http://localhost:3000">홈으로</a>
       <div>
         <h4>useState 방식 : {normal}</h4>
-        <button onClick={decreaseZNormal}>-</button>
-        <button onClick={increaseZNormal}>+</button>
+        <button onClick={decreaseNormal}>-</button>
+        <button onClick={increaseNormal}>+</button>
       </div>
       <div>
         <h4>useState 방식 : {zNormal}</h4>
         <button onClick={decreaseZNormal}>-</button>
         <button onClick={increaseZNormal}>+</button>
       </div>
+      <SubComponent1 normal={normal} increaseNormal={increaseNormal} decreaseNormal={decreaseNormal} />
+      <SubComponent2 />
+    </div>
+  )
+}
+
+interface Sub1Props {
+  normal: number,
+  increaseNormal: () => void,
+  decreaseNormal: () => void
+}
+
+function SubComponent1 ({ normal, increaseNormal, decreaseNormal}: Sub1Props) {
+
+  return (
+    <div>
+      <h5>Normal : {normal}</h5>
+      <button onClick={decreaseNormal}>-</button>
+      <button onClick={increaseNormal}>+</button>
+    </div>
+  )
+}
+
+function SubComponent2 () {
+
+  const { zNormal, increaseZNormal, decreaseZNormal } = useStore();
+
+  return (
+    <div>
+      <h5>Zustand : {zNormal}</h5>
+      <button onClick={decreaseZNormal}>-</button>
+      <button onClick={increaseZNormal}>+</button>
     </div>
   )
 }
