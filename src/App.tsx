8c64226549559956ea548614de66ce33b25e6bd6
ahndb <asi0000@nate.com>
2024-04-18 12:15:04 +0900

    240418 11:50 Update Axios(params, pathname)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ZustandLibrary from 'src/zustand/ZustandLibrary';
 import ReactRouterLibrary, { ReactRouterPage1, ReactRouterPage2 } from './react-router/ReactRouterLibrary';
 import { Route, Routes } from 'react-router';
 import Layout from './react-router/Layout';
+import AsioxLibrary from './axios/AsioxLibrary';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='page2' element={<ReactRouterPage2 />} />
       </Route>
       <Route path='/zustand' element={<ZustandLibrary />} />
+      <Route path='/axios' element={<AsioxLibrary />} />
       {/* 사용하지 않는 페이지도 아닌페이지라고 명시해서 관리해야함 */}
       <Route path='*' element={<h3>404 Error</h3>} />
     </Routes>
---
+++ src/react-cookie/ReactCookieLibrary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//! react-cookie : 
+//? React에서 쿠키관리를 수월하게하는 라이브러리
+// npm i react-cookie
+
+export default function ReactCookieLibrary() {
+  return (
+    <div>ReactCookieLibrary</div>
+  )
+}
